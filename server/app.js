b99068a33ace4c1d39b53b8b7e29835eed759cf4
dbaluyut <derriquebaluyut@gmail.com>
2020-10-12 14:53:57 -0700

    init commit

--- server/app.js
+++ server/app.js
@@ -1,13 +1,19 @@
-const express = require('express')
+import axios from "axios"
+
+const express = require("express")
 const app = express()
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.json({hello: true})
+function getRandomPersonfromApi() {
+  axios.get(`https://randomuser.me/api/?nat=gb`).then((resp) => resp.data)
+}
+
+app.get("/", (req, res) => {
+  res.json(getRandomPersonfromApi())
 })
 
 app.listen(3001, (req, res) => {
-    console.log('listening on port 3001')
+  console.log("listening on port 3001")
 })
